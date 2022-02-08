91bbe1c5b3381e688123934eea21cd748be3d45c
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2022-02-08 23:08:18 +0530

    Update and rename Exercise9.java to Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,59 @@
+
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button add = (Button) findViewById(R.id.add);
+        Button sub = (Button) findViewById(R.id.sub);
+        Button mul = (Button) findViewById(R.id.mul);
+        Button div = (Button) findViewById(R.id.div);
+
+        EditText num1 = (EditText) findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        EditText result = (EditText) findViewById(R.id.result);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(num1.getText().toString());
+                int b = Integer.parseInt(num2.getText().toString());
+                result.setText(Integer.toString(a+b));
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(num1.getText().toString());
+                int b = Integer.parseInt(num2.getText().toString());
+                result.setText(Integer.toString(a-b));
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(num1.getText().toString());
+                int b = Integer.parseInt(num2.getText().toString());
+                result.setText(Integer.toString(a*b));
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(num1.getText().toString());
+                int b = Integer.parseInt(num2.getText().toString());
+                result.setText(Integer.toString(a/b));
+            }
+        });
+    }
+}
