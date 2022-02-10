b049effa60ac3f7b9f679388d62bbb473f73e19d
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2022-02-10 15:48:39 +0530

    Create MainActivity.java

---
+++ Exercise12/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.activitynavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
