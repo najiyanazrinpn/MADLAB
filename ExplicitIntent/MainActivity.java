50725ad280d76f1c91c94731660b2aa00c5b8ed3
Najiya Nazrin P N <71110070+najiyanazrinpn@users.noreply.github.com>
2022-02-14 15:49:21 +0530

    Create MainActivity.java

---
+++ ExplicitIntent/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("value", "Hi");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
